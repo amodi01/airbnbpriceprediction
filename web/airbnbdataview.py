a6c6b718bcfd4cb7416939bc8d5dca3087ee45fc
amodi01 <amitmodi.79@gmail.com>
2021-02-12 16:11:49 +0530

    added code for Stramlit

--- web/airbnbdataview.py
+++ web/airbnbdataview.py
@@ -3,5 +3,33 @@ import streamlit as st
 # working with sample data.
 import numpy as np
 import pandas as pd
+import pickle5 as pickle
 
 st.title("Airbnb NYC Data Jan-2020- Dec2-020")
+#File Location Variables
+_LocationName="NY"
+_DataFolderPath="raw_data_csv"
+_LocationPath=_DataFolderPath +"/" + _LocationName
+_PickleFilesFolder="pickle_files"
+_PickleFile_Merged_Listing_NY="Merged_Listing_NY_4"
+
+#Reading Pickle File
+@st.cache
+def load_data(nrows):
+    with open(_PickleFile_Merged_Listing_NY,'rb') as f:
+        _DF_LISTING_EDA = pickle.load(f)
+    return _DF_LISTING_EDA.head(nrows)
+
+data_load_state=st.text("Loading Data....")
+data=load_data(100)
+data_load_state.text("Loading Data ..Done!!")
+
+is_check = st.checkbox("Display Data")
+if is_check:
+    st.write(data)
+
+
+st.bar_chart(data.groupby('property_type')['price'].mean())
+
+st.bar_chart(data.groupby('room_type')['price'].mean())
+
